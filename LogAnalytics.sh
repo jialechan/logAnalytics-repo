1c3fd1af038f7ea584b4cb92648a1120fdf6b929
Jiale.Chan <jiale.chan@gmail.com>
2017-11-28 14:34:23 +0800

    Update LogAnalytics.sh

--- LogAnalytics.sh
+++ LogAnalytics.sh
@@ -6,6 +6,6 @@ cd /userdata1/server_script/logAnalytics-repo
 
 git pull
 
-chmod +x LogAnalytics.jar jq LogAnalytics.sh log_daily.sh nginxConfig.sh
+chmod +x LogAnalytics.jar LogAnalytics.sh log_daily.sh nginxConfig.sh
 
 /userdata1/software/jdk1.8/bin/java -jar ./LogAnalytics.jar --dateStr=`date -d yesterday +%Y-%m-%d`
