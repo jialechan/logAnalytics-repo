aec4ae7a3d186a7d1138411153211a38fbb06a96
jialechan <jiale.chen@afmobigroup.com>
2017-12-02 00:31:07 +0800

    init

--- LogAnalytics.sh
+++ LogAnalytics.sh
@@ -6,6 +6,4 @@ cd /userdata1/server_script/logAnalytics-repo
 
 git pull
 
-chmod +x LogAnalytics.jar LogAnalytics.sh log_daily.sh nginxConfig.sh
-
 /userdata1/software/jdk1.8/bin/java -jar ./LogAnalytics.jar --dateStr=`date -d yesterday +%Y-%m-%d`
